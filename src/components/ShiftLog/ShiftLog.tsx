f02f4a04cae1c813f3ed8d09649647d7975c4bbf
Omprakash Verma <omprakash_verma@persistent.com>
2024-06-20 10:42:17 +0530

    test helm buils

--- src/components/ShiftLog/ShiftLog.tsx
+++ src/components/ShiftLog/ShiftLog.tsx
@@ -1,87 +1,76 @@
-import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import {  Container, Grid, Paper, TextField, Typography } from '@mui/material';
-import { Button, ButtonColorTypes, ButtonVariantTypes, TextEntry } from '@ska-telescope/ska-gui-components';
-import { FileUpload } from '@ska-telescope/ska-gui-components';
+import { Grid, Paper, TextField } from '@mui/material';
+import {
+  Button,
+  ButtonColorTypes,
+  ButtonVariantTypes,
+  FileUpload
+} from '@ska-telescope/ska-gui-components';
 
 const ShiftLog = () => {
   const { t } = useTranslation('translations');
-  const [id, setId] = useState<string>('');
 
-  
   return (
-    <>
-     <Paper sx={{pb:2}}>
-
-      <Grid container sx={{ margin: 2 }} justifyContent="left" >
-
-      <Grid item xs={12} sm={12} md={2} >
-        <Button
-            disabled = {true}
-            color={ButtonColorTypes.Secondary}
-            variant={ButtonVariantTypes.Contained}
-            testId="dateSearch"
-            label='Shift started'
-            toolTip={t('toolTip.button.dateSearch')}
-          />
-          </Grid>
-          <Grid item xs={12} sm={12} md={3}>
+    <Paper sx={{ pb: 2 }}>
+      <Grid container sx={{ margin: 2 }} justifyContent="left">
+        <Grid item xs={12} sm={12} md={2}>
           <Button
-               disabled={false}
+            disabled
             color={ButtonColorTypes.Secondary}
             variant={ButtonVariantTypes.Contained}
             testId="dateSearch"
-            label='End shift'
+            label="Shift started"
             toolTip={t('toolTip.button.dateSearch')}
           />
-            
-            </Grid>
-            <Grid item xs={12} sm={12} md={5}></Grid>
-            <Grid item xs={12} sm={12} md={2}>
+        </Grid>
+        <Grid item xs={12} sm={12} md={3}>
+          <Button
+            disabled={false}
+            color={ButtonColorTypes.Secondary}
+            variant={ButtonVariantTypes.Contained}
+            testId="dateSearch"
+            label="End shift"
+            toolTip={t('toolTip.button.dateSearch')}
+          />
+        </Grid>
+        <Grid item xs={12} sm={12} md={5} />
+        <Grid item xs={12} sm={12} md={2}>
           <Button
             color={ButtonColorTypes.Inherit}
             variant={ButtonVariantTypes.Contained}
             testId="dateSearch"
-            label='previous shifts'
+            label="previous shifts"
             toolTip={t('toolTip.button.dateSearch')}
           />
-          </Grid>
-  
-          </Grid>
-          <Grid container sx={{ margin: 2 }} justifyContent="left" >
-      <Grid item xs={12} sm={12} md={5} >
-      <TextField
-       sx={{width: 500}}
-  id="outlined-multiline-static"
-  label="Comments"
-  multiline
-  rows={4}
-/>
         </Grid>
-        <Grid item xs={12} sm={12} md={1} >
-           <Button
-               disabled={false}
+      </Grid>
+      <Grid container sx={{ margin: 2 }} justifyContent="left">
+        <Grid item xs={12} sm={12} md={5}>
+          <TextField
+            sx={{ width: 500 }}
+            id="outlined-multiline-static"
+            label="Comments"
+            multiline
+            rows={4}
+          />
+        </Grid>
+        <Grid item xs={12} sm={12} md={1}>
+          <Button
+            disabled={false}
             color={ButtonColorTypes.Secondary}
             variant={ButtonVariantTypes.Contained}
             testId="dateSearch"
-            label='Save'
+            label="Save"
             toolTip={t('toolTip.button.dateSearch')}
           />
-           </Grid>
-
-           <Grid item xs={12} sm={12} md={5} >
-           <FileUpload direction = "row"/>
-           </Grid>
- 
         </Grid>
-      
+
+        <Grid item xs={12} sm={12} md={5}>
+          <FileUpload direction="row" />
+        </Grid>
+      </Grid>
     </Paper>
-    </>
-  )
+  );
 };
 
 export default ShiftLog;
-function searchClicked(arg0: string, value: string) {
-  throw new Error('Function not implemented.');
-}
-
