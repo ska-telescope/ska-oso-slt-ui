cb80ebb277d30873649cda4b97cd7c24145342df
Omprakash Verma <omprakash_verma@persistent.com>
2024-06-20 10:54:57 +0530

    test helm buils

--- src/components/ShiftLog/ShiftLog.tsx
+++ src/components/ShiftLog/ShiftLog.tsx
@@ -1,76 +1,10 @@
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Grid, Paper, TextField } from '@mui/material';
-import {
-  Button,
-  ButtonColorTypes,
-  ButtonVariantTypes,
-  FileUpload
-} from '@ska-telescope/ska-gui-components';
 
 const ShiftLog = () => {
   const { t } = useTranslation('translations');
 
-  return (
-    <Paper sx={{ pb: 2 }}>
-      <Grid container sx={{ margin: 2 }} justifyContent="left">
-        <Grid item xs={12} sm={12} md={2}>
-          <Button
-            disabled
-            color={ButtonColorTypes.Secondary}
-            variant={ButtonVariantTypes.Contained}
-            testId="dateSearch"
-            label="Shift started"
-            toolTip={t('toolTip.button.dateSearch')}
-          />
-        </Grid>
-        <Grid item xs={12} sm={12} md={3}>
-          <Button
-            disabled={false}
-            color={ButtonColorTypes.Secondary}
-            variant={ButtonVariantTypes.Contained}
-            testId="dateSearch"
-            label="End shift"
-            toolTip={t('toolTip.button.dateSearch')}
-          />
-        </Grid>
-        <Grid item xs={12} sm={12} md={5} />
-        <Grid item xs={12} sm={12} md={2}>
-          <Button
-            color={ButtonColorTypes.Inherit}
-            variant={ButtonVariantTypes.Contained}
-            testId="dateSearch"
-            label="previous shifts"
-            toolTip={t('toolTip.button.dateSearch')}
-          />
-        </Grid>
-      </Grid>
-      <Grid container sx={{ margin: 2 }} justifyContent="left">
-        <Grid item xs={12} sm={12} md={5}>
-          <TextField
-            sx={{ width: 500 }}
-            id="outlined-multiline-static"
-            label="Comments"
-            multiline
-            rows={4}
-          />
-        </Grid>
-        <Grid item xs={12} sm={12} md={1}>
-          <Button
-            disabled={false}
-            color={ButtonColorTypes.Secondary}
-            variant={ButtonVariantTypes.Contained}
-            testId="dateSearch"
-            label="Save"
-            toolTip={t('toolTip.button.dateSearch')}
-          />
-        </Grid>
-
-        <Grid item xs={12} sm={12} md={5}>
-          <FileUpload direction="row" />
-        </Grid>
-      </Grid>
-    </Paper>
-  );
+  return <h2> {t('text.title')}</h2>;
 };
 
 export default ShiftLog;
